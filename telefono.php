31085a5b9b8d6c0c0755804680ac3eaa1fc0a341
farmijoss1995 <marceloas@hotmail.es>
2020-03-30 15:59:53 -0500

    telefono.php

---
+++ telefono.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<title>MANGOBOUTIQUE</title>
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<link href="styles.css" rel="stylesheet" type="text/css" media="screen" />
+</head>
+<body>
+
+<div id="bg">
+
+<!-- header begins -->
+<div id="header">
+	<div id="logo">
+        <img src="images/2.png">
+        <h1><a href="#">MANGO BOUTIQUE</a>
+        </h1></div>
+	<div id="buttons">
+		<ul>
+			<li class="first"><a href="index.php"  title="">Inicio</a></li>
+			<li><a href="nosotros.php" title="">Nosotros</a></li>
+			<li><a href="telefono.php" title="">Telefonos</a></li>
+			<li><a href="locales.php" title="">Locales</a></li>
+            <li><a href="iniciarprincipal.php" title="">Iniciar Sesión</a></li>
+		</ul>
+	</div>
+</div>
+<!-- header ends -->
+
+<div id="main">
+<!--<div id="maintop"><img src="images/spaser.gif" alt="" /></div>
+<!-- content begins -->
+<div id="content">
+	<div id="right">
+    	<div id="archives">
+
+
+        </div>
+     </div>
+        <center>
+         <div id="left">
+        <div class="text">
+
+                <div style="background: white">Cotocollao 022 290290<br>
+                    <br>
+El Bosque 022 257449<br>
+                    <br>
+Hospital IESS 022 558645<br>
+                    <br>
+El Inca 022 245777<br>
+                    <br>
+Cumbaya 022 891866<br>
+                    <br>
+Hospital Baca Ortiz  022 245777<br>
+                    <br>
+
+</div>
+
+
+
+
+     </div>
+	 </div>
+            </center>
+
+
+
+
+
+	<div style="clear: both"></div>
+
+</div>
+
+<!-- content ends -->
+<!--<div id="mainbot"><img src="images/spaser.gif" alt="" /></div>-->
+</div>
+
+</div>
+</body>
+</html>
