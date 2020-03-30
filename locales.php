26d78f76f5f1a7a2543f3fc06539ef19fb35da49
farmijoss1995 <marceloas@hotmail.es>
2020-03-30 16:02:56 -0500

    locales.php

---
+++ locales.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<title>BELLY SATISFIED</title>
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<link href="styles.css" rel="stylesheet" type="text/css" media="screen" />
+</head>
+<body>
+
+<div id="bg">
+
+<!-- header begins -->
+<div id="header">
+	<div id="logo">
+        <img src="images/2.png">
+        <h1><a href="#">MANGO BOUTIQUE</a>
+        </h1></div>
+	<div id="buttons">
+		<ul>
+			<li class="first"><a href="index.php"  title="">Inicio</a></li>
+			<li><a href="nosotros.php" title="">Nosotros</a></li>
+			<li><a href="telefono.php" title="">Telefonos</a></li>
+			<li><a href="locales.php" title="">Locales</a></li>
+            <li><a href="iniciarprincipal.php" title="">Iniciar Sesión</a></li>
+		</ul>
+	</div>
+</div>
+<!-- header ends -->
+
+<div id="main">
+<!--<div id="maintop"><img src="images/spaser.gif" alt="" /></div>
+<!-- content begins -->
+<div id="content">
+	<div id="right">
+    	<div id="archives">
+
+
+        </div>
+     </div>
+        <center>
+         <div id="left">
+        <div class="text">
+
+                <div style="background: white">CUENCA<br>
+AV. JOSE PERALTA S <br>
+Horario:11:00 - 21:15 <br>
+074103393<br>
+
+
+                    <br>
+                    <br>
+MONAY<br>
+AV. GONZALEZ SUAREZ S<br>
+Horario:Sin Horario<br>
+23955400<br>
+                    <br>
+                    <br>
+EL BATAN SHOPPING CUENCA<br>
+Av. Remigio Crespo y Santa Cruz<br>
+Horario:Sin Horario<br>
+23955400</div>
+
+
+
+
+     </div>
+	 </div>
+            </center>
+
+
+
+
+
+	<div style="clear: both"></div>
+
+</div>
+
+<!-- content ends -->
+<!--<div id="mainbot"><img src="images/spaser.gif" alt="" /></div>-->
+</div>
+
+</div>
+</body>
+</html>
