75a912f85f0c50f069f5f8816f4b6b1d3532deac
farmijoss1995 <marceloas@hotmail.es>
2020-03-30 16:30:35 -0500

    cerrar.php

---
+++ cerrar.php
@@ -0,0 +1,23 @@
+<?php
+include 'noingresar.php';
+?>
+<?php
+session_start();
+session_destroy();
+echo 'cerraste sesion';
+echo '<script>window.location="index.php";</script>';
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Saliendo...</title>
+        <meta charset="utf-8">
+    </head>
+
+    <body>
+        <script language="javascript">location.href="index.php";
+        </script>
+    </body>
+
+</html>
