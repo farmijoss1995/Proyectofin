fe10c96425245b84dd3c0620907af82f7daca646
farmijoss1995 <marceloas@hotmail.es>
2020-03-30 16:02:49 -0500

    iniciarprincipal.php

---
+++ iniciarprincipal.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<title>MANGOBOUTIQUE</title>
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<link href="styles.css" rel="stylesheet" type="text/css" media="screen" />
+</head>
+<body style="margin-top: 7%">
+
+<div id="bg">
+
+<div id="main">
+<div id="maintop">
+<center>
+<!--img src="images/44.png" alt="" /></div></center>
+<!-- content begins -->
+<div id="content">
+
+        <center>
+            <div id="maintop">
+            <br>
+             <br>
+             <br>
+
+
+            <h1 style="background: blue">INICIAR SESIÓN</h1>
+            <img src="images/44.png" alt="" /></div></center>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br> <br>
+            <br>
+            <br>
+            <a style="background: black" href="iniciaradm.php">Administrador</a>
+
+
+
+            <a style="background: black" href="iniciar.php">Cliente</a>
+
+            <br>
+            <br>
+            <br>
+
+
+            <input style="background: black" type="button" name="cancelar" value="Cancelar" onclick="location='index.php'">
+             <br>
+             <br>
+
+
+
+
+            </center>
+
+
+
+
+
+	<div style="clear: both"></div>
+
+</div>
+
+</div>
+
+
+</div>
+</body>
+</html>
+
+
+
