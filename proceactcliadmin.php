74c97f69f1a34a2e40ee6388f357975eec0fd0f7
farmijoss1995 <marceloas@hotmail.es>
2020-03-30 16:35:33 -0500

    proceactcliadmin.php

---
+++ proceactcliadmin.php
@@ -0,0 +1,25 @@
+<?php
+
+include 'conexion.php';
+
+$id=$_REQUEST['id'];
+
+$nombres= $_POST['nombres'];
+$apellidos= $_POST['apellidos'];
+$correo= $_POST['correo'];
+$telefono= $_POST['telefono'];
+$direccion= $_POST['direccion'];
+$contrasena= $_POST['contrasena'];
+
+$query= "UPDATE cliente SET nombres='$nombres',
+apellidos='$apellidos', correo='$correo', telefono='$telefono', direccion='$direccion', contrasena='$contrasena' WHERE id='$id'";
+
+$resultado= $conexion->query($query);
+
+if($resultado){
+  header("Location: gestionclientes.php");
+}else{
+    echo "insercion no exitosa";
+}
+
+?>
