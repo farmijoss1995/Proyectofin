84fd167a1f0fc19c4efba7573dd6a99563308746
farmijoss1995 <marceloas@hotmail.es>
2020-03-30 14:09:03 -0500

    index.php

---
+++ index.php
@@ -0,0 +1,197 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<title>MANGO BOUTIQUE</title>
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<link href="styles.css" rel="stylesheet" type="text/css" media="screen" />
+</head>
+<body>
+
+<div id="bg">
+
+<!-- header begins -->
+<div id="header">
+	<div id="logo">
+        <img src="images/2.png">
+        <h1><a href="#">MANGO BOUTIQUE</a></h1></div>
+	<div id="buttons">
+		<ul>
+            <br>
+			<li class="first"><a href="#"  title="">Inicio</a></li>
+			<li><a href="nosotros.php" title="">Nosotros</a></li>
+			<li><a href="telefono.php" title="">Telefonos</a></li>
+			<li><a href="locales.php" title="">Locales</a></li>
+			<li><a href="iniciarprincipal.php" title="">Iniciar Sesión</a></li>
+		</ul>
+	</div>
+</div>
+<!-- header ends -->
+
+<div id="main">
+<!--div id="maintop"><img src="images/1.png" alt="" /></div> -->
+<content begins >
+<div id="content">
+	<div id="right">
+    	<div id="archives">
+
+
+               <img src="images/" alt="" /></div>
+                <div class="tit_b">
+                    <ul>
+                        <br>
+                        <br>
+                        <br>
+                        <br>
+                        <br>
+                        <br>
+                        <br>
+                        <br>
+                        <br>
+                        <br>
+                        <br>
+                        <br>
+                        <br>
+                        <br>
+                    </ul>
+
+                </div>
+				<h2>Calendario</h2>
+                <div class="tit_b">
+                      <div id="calendar">
+                        <table id="calendar2" summary="Calendar">
+                          <caption>
+                            Agosto 2018
+                          </caption>
+                          <thead>
+                            <tr>
+                              <th abbr="Monday" scope="col" title="Monday">L</th>
+                              <th abbr="Tuesday" scope="col" title="Tuesday">M</th>
+                              <th abbr="Wednesday" scope="col" title="Wednesday">M</th>
+                              <th abbr="Thursday" scope="col" title="Thursday">J</th>
+                              <th abbr="Friday" scope="col" title="Friday">V</th>
+                              <th abbr="Saturday" scope="col" title="Saturday">S</th>
+                              <th abbr="Sunday" scope="col" title="Sunday">D</th>
+                            </tr>
+                          </thead>
+                          <tfoot>
+                            <tr>
+                              <td abbr="Des" colspan="3" id="prev"><a href="#">&laquo; Jul</a></td>
+                              <td class="pad">&nbsp;</td>
+                              <td abbr="Feb" colspan="3" id="next" class="pad"><a href="#">Sep &raquo;</a></td>
+                            </tr>
+                          </tfoot>
+                          <tbody>
+                            <tr>
+                              <td colspan="2" class="pad">&nbsp;</td>
+                              <td>1</td>
+                              <td>2</td>
+                              <td>3</td>
+                              <td>4</td>
+                              <td>5</td>
+                            </tr>
+                            <tr>
+                              <td id="now">6</td>
+                              <td>7</td>
+                              <td>8</td>
+                              <td>9</td>
+                              <td>10</td>
+                              <td>11</td>
+                              <td>12</td>
+                            </tr>
+                            <tr>
+                              <td>13</td>
+                              <td>14</td>
+                              <td>15</td>
+                              <td>16</td>
+                              <td>17</td>
+                              <td>18</td>
+                              <td>19</td>
+                            </tr>
+                            <tr>
+                              <td>20</td>
+                              <td>21</td>
+                              <td>22</td>
+                              <td>23</td>
+                              <td>24</td>
+                              <td>25</td>
+                              <td>26</td>
+                            </tr>
+                            <tr>
+                              <td>27</td>
+                              <td>28</td>
+                              <td>29</td>
+                              <td>30</td>
+                              <td>31</td>
+                              <td class="pad" colspan="2">&nbsp;</td>
+                            </tr>
+                          </tbody>
+                        </table>
+                      </div>
+          		</div>
+        </div>
+     </div>
+        <center>
+         <div id="left">
+        <div class="text">
+            <br>
+            <br>
+            <br>
+            <br>
+		<h1>HAZ TU PEDIDO EN LÍNEA</h1>
+            <br>
+            <br>
+            <h3 style="font-size: 25px">Cuenca-Azuay-Ecuador</h3>
+            <br>
+            <br>
+            <input style="background: black" type="submit" name="reguistro" id="reguistrar" value="Regístrate aquí" onclick="location='clientes.php'">
+     </div>
+	 </div>
+            </center>
+
+
+
+
+
+	<div style="clear: both"></div>
+
+</div>
+
+<!-- content ends -->
+<!--div id="mainbot"><img src="images/1.png" alt="" /></div>
+</div>-->
+
+    <footer style="background:black" >
+        <br>
+        <center>
+<h2 style="color: white", align>Contactanos</h2>
+        <p style="color: white">Siguenos en nuestras <br>
+                                redes sociales</p>
+        <input style="background: white" type="text" name="co" id="cor" value="" placeholder="Email">
+
+        <br>
+        <br>
+        <center>
+        <textarea name="comentarios" rows="5" cols="25" placeholder="Mensaje"></textarea> </center>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;<br>
+        <input style="background: #F5F6CE" type="submit" name="enviar" id="enviar" value="Enviar" onClick="../Proyecto final/clientes.php">
+        <br><br>
+
+        <a href="https://www.facebook.com/" target="_blank">
+        <img src="images/f.png"></a> &nbsp; &nbsp;
+        <a href="https://www.instagram.com/" target="_blank">
+        <img src="images/i.png"></a> &nbsp; &nbsp;
+        <a href="https://twitter.com/" target="_blank">
+
+        <img src="images/t.png"></a>
+
+        <br>
+        <br>
+
+    </center>
+    </footer>
+
+
+</div>
+</body>
+</html>
