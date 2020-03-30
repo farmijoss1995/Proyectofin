7a66b1ff83c2c52404f90390f3102ef2fd24dd7a
farmijoss1995 <marceloas@hotmail.es>
2020-03-30 15:58:33 -0500

    nosotros.php

---
+++ nosotros.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<title>MANGO BOUTIQUE</title>
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<link href="styles.css" rel="stylesheet" type="text/css" media="screen" />
+</head>
+<body>
+
+<div id="bg">
+
+<!-- header begins -->
+<div id="header">
+	<div id="logo">
+       <img src="images/2.png">
+
+        <h1><a href="#">MANGO BOUTIQUE</a>
+        </h1></div>
+	<div id="buttons">
+		<ul>
+			<li class="first"><a href="index.php"  title="">Inicio</a></li>
+			<li><a href="nosotros.php" title="">Nosotros</a></li>
+			<li><a href="telefono.php" title="">Telefonos</a></li>
+			<li><a href="locales.php" title="">Locales</a></li>
+            <li><a href="iniciarprincipal.php" title="">Iniciar Sesión</a></li>
+		</ul>
+	</div>
+</div>
+<!-- header ends -->
+
+<div id="main">
+<div id="maintop">
+<!--img src="images/1.png" alt="" /></div>
+<!-- content begins -->
+<div id="content">
+	<div id="right">
+    	<div id="archives">
+
+
+        </div>
+     </div>
+        <center>
+         <div id="left">
+        <div class="text">
+
+                <div style="background: white">SOMOS LAS MEJORES MARCAS DE ROPA EN MANGO
+                    <br>
+                    <br>
+MANGO es una multinacional de prestigio internacional dedicada al diseño, la fabricación y la comercialización de prendas de vestir y complementos para la mujer y el hombre.
+                    <br>
+                    <br>
+El Hangar Design Center, el centro de diseño más grande de Europa, cuenta con una superficie de 10.000 m2 que acoge a más de 600 profesionales dedicados a la creación de moda y complementos para la mujer y el hombre. En él se encuentran las áreas de diseño, compras y calidad.
+
+                    <br>
+                    <br>
+
+Actualmente, MANGO cuenta con más de 2.200 puntos de venta en 109 países a lo largo del mundo.
+
+                    <br>
+                    <br>
+
+MANGO basa su logística en un sistema propio que ha ido desarrollando progresivamente desde la apertura de nuestra primera tienda en el año 1984 hasta la actualidad, en que MANGO se ha constituido como la segunda empresa exportadora del sector textil español.
+</div>
+
+
+
+
+     </div>
+	 </div>
+            </center>
+
+
+
+
+	<div style="clear: both"></div>
+
+</div>
+
+<!-- content ends -->
+<!--<div id="mainbot"><img src="images/spaser.gif" alt="" /></div>-->
+</div>
+
+</div>
+</body>
+</html>
+
