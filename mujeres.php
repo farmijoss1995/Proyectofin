40b973527f6cadd777f9c8951a71c226f470a66d
farmijoss1995 <marceloas@hotmail.es>
2020-04-03 20:17:32 -0500

    mujeres.php

---
+++ mujeres.php
@@ -0,0 +1,295 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+   <title>MODAMUJERES</title>
+    <meta charset="UTF-8">
+
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1, maximun-scale=1, minimun-scale=1" />
+    <link rel="stylesheet" href="estilos.css" />
+
+</head>
+<body>
+   <div id="header">
+	<div id="logo">
+        <img src="images/2.png">
+        <h1><a href="#">MANGO BOUTIQUE</a></h1></div>
+	<div id="buttons">
+		<ul>
+            <br>
+			<li class="first"><a href="index.php"  title="">Inicio</a></li>
+			<li><a href="mujeres.php" title="">Mujeres</a></li>
+
+
+
+
+		</ul>
+	</div>
+</div>
+    <h1>CATALOGO DE ROPA PARA HOMBRE</h1>
+
+    <ul class="galeria">
+        <li><a href="#img1"><img src="img/21.png"></a></li>
+        <li><a href="#img2"><img src="img/22.png"></a></li>
+        <li><a href="#img3"><img src="img/23.png"></a></li>
+        <li><a href="#img4"><img src="img/24.png"></a></li>
+        <li><a href="#img5"><img src="img/25.png"></a></li>
+        <li><a href="#img6"><img src="img/26.png"></a></li>
+        <li><a href="#img7"><img src="img/27.png"></a></li>
+        <li><a href="#img8"><img src="img/28.png"></a></li>
+        <li><a href="#img9"><img src="img/29.png"></a></li>
+        <li><a href="#img10"><img src="img/30.png"></a></li>
+        <li><a href="#img11"><img src="img/29.png"></a></li>
+        <li><a href="#img12"><img src="img/28.png"></a></li>
+        <li><a href="#img13"><img src="img/27.png"></a></li>
+        <li><a href="#img14"><img src="img/26.png"></a></li>
+        <li><a href="#img15"><img src="img/25.png"></a></li>
+        <li><a href="#img16"><img src="img/24.png"></a></li>
+        <li><a href="#img17"><img src="img/23.png"></a></li>
+        <li><a href="#img18"><img src="img/22.png"></a></li>
+        <li><a href="#img19"><img src="img/21.png"></a></li>
+        <li><a href="#img20"><img src="img/30.png"></a></li>
+
+    </ul>
+
+ <div class="modal" id="img1">
+     <h3><marquee bgcolor="black" behavior="alternate">SUETER AZUL OBSCURO 25$</marquee></h3>
+       <h3><marquee bgcolor="black" behavior="alternate">PANTALON DE TELA AZUL CLARO 45$</marquee></h3>
+       <h3><marquee bgcolor="black" behavior="alternate">ZAPATOS ACTUR AZUL 35$</marquee></h3>
+
+         <div class="imagen">
+            <a href="#img20">&#60;</a>
+            <a href="#img2"><img src="img/21.png" ></a>
+            <a href="#img2">></a>
+         </div>
+          <a class="cerrar" href="">X</a>
+
+
+
+         <div class="modal" id="img2">
+     <h3><marquee bgcolor="black" behavior="alternate">SACO GRIS BARDOCK 55$</marquee></h3>
+       <h3><marquee bgcolor="black" behavior="alternate">GAFAS DIESEL CAFES 25$</marquee></h3>
+       <h3><marquee bgcolor="black" behavior="alternate">PANTALON DE VESTIR CAFE CLARO 45$</marquee></h3>
+
+         <div class="imagen">
+            <a href="#img1">&#60;</a>
+            <a href="#img3"><img src="img/22.png" ></a>
+            <a href="#img3">></a>
+         </div>
+          <a class="cerrar" href="">X</a>
+
+
+    <div class="modal" id="img3">
+     <h3><marquee bgcolor="black" behavior="alternate">CHOMPA GRIS 25$</marquee></h3>
+       <h3><marquee bgcolor="black" behavior="alternate">PANTALON DE TELA NEGRO OBSCURO 35$</marquee></h3>
+       <h3><marquee bgcolor="black" behavior="alternate">ZAPATOS ACTUR NEGROS 35$</marquee></h3>
+
+         <div class="imagen">
+            <a href="#img2">&#60;</a>
+            <a href="#img4"><img src="img/23.png" ></a>
+            <a href="#img4">></a>
+         </div>
+          <a class="cerrar" href="">X</a>
+
+     <div class="modal" id="img4">
+
+       <h3><marquee bgcolor="black" behavior="alternate">TERNO COMPLETO AZUL DIESEL 355$</marquee></h3>
+
+
+         <div class="imagen">
+            <a href="#img3">&#60;</a>
+            <a href="#img5"><img src="img/24.png" ></a>
+            <a href="#img5">></a>
+         </div>
+          <a class="cerrar" href="">X</a>
+
+
+         <div class="modal" id="img5">
+     <h3><marquee bgcolor="black" behavior="alternate">CHAQUETA CAFE CLARO 30$</marquee></h3>
+       <h3><marquee bgcolor="black" behavior="alternate">PANTALON JEAN AZUL CLARO 305$</marquee></h3>
+       <h3><marquee bgcolor="black" behavior="alternate">ZAPATOS ALDEN AZUL 35$</marquee></h3>
+
+         <div class="imagen">
+            <a href="#img4">&#60;</a>
+            <a href="#img6"><img src="img/25.png" ></a>
+            <a href="#img6">></a>
+         </div>
+          <a class="cerrar" href="">X</a>
+
+
+
+        <div class="modal" id="img6">
+     <h3><marquee bgcolor="black" behavior="alternate">SUETER CAFE CLARO 25$</marquee></h3>
+       <h3><marquee bgcolor="black" behavior="alternate">PANTALON DE TELA CAFE 45$</marquee></h3>
+       <h3><marquee bgcolor="black" behavior="alternate">ZAPATOS ALDO NEGRO 35$</marquee></h3>
+
+         <div class="imagen">
+            <a href="#img5">&#60;</a>
+            <a href="#img7"><img src="img/26.png" ></a>
+            <a href="#img7">></a>
+         </div>
+          <a class="cerrar" href="">X</a>
+
+
+    <div class="modal" id="img7">
+     <h3><marquee bgcolor="black" behavior="alternate">SUETER AZUL OBSCURO 25$</marquee></h3>
+       <h3><marquee bgcolor="black" behavior="alternate">PANTALON DE TELA AZUL CLARO 45$</marquee></h3>
+       <h3><marquee bgcolor="black" behavior="alternate">ZAPATOS ACTUR AZUL 35$</marquee></h3>
+
+         <div class="imagen">
+            <a href="#img6">&#60;</a>
+            <a href="#img8"><img src="img/27.png" ></a>
+            <a href="#img8">></a>
+         </div>
+          <a class="cerrar" href="">X</a>
+
+     <div class="modal" id="img8">
+     <h3><marquee bgcolor="black" behavior="alternate">SUETER AZUL OBSCURO 25$</marquee></h3>
+       <h3><marquee bgcolor="black" behavior="alternate">PANTALON DE TELA AZUL CLARO 45$</marquee></h3>
+       <h3><marquee bgcolor="black" behavior="alternate">ZAPATOS ACTUR AZUL 35$</marquee></h3>
+
+         <div class="imagen">
+            <a href="#img7">&#60;</a>
+            <a href="#img9"><img src="img/28.png" ></a>
+            <a href="#img9">></a>
+         </div>
+          <a class="cerrar" href="">X</a>
+
+         <div class="modal" id="img9">
+     <h3><marquee bgcolor="black" behavior="alternate">SUETER AZUL OBSCURO 25$</marquee></h3>
+       <h3><marquee bgcolor="black" behavior="alternate">PANTALON DE TELA AZUL CLARO 45$</marquee></h3>
+       <h3><marquee bgcolor="black" behavior="alternate">ZAPATOS ACTUR AZUL 35$</marquee></h3>
+
+         <div class="imagen">
+            <a href="#img8">&#60;</a>
+            <a href="#img10"><img src="img/29.png" ></a>
+            <a href="#img10">></a>
+         </div>
+          <a class="cerrar" href="">X</a>
+
+         <div class="modal" id="img10">
+     <h3><marquee bgcolor="black" behavior="alternate">SUETER AZUL OBSCURO 25$</marquee></h3>
+       <h3><marquee bgcolor="black" behavior="alternate">PANTALON DE TELA AZUL CLARO 45$</marquee></h3>
+       <h3><marquee bgcolor="black" behavior="alternate">ZAPATOS ACTUR AZUL 35$</marquee></h3>
+
+         <div class="imagen">
+            <a href="#img9">&#60;</a>
+            <a href="#img11"><img src="img/30.png" ></a>
+            <a href="#img11">></a>
+         </div>
+          <a class="cerrar" href="">X</a>
+
+         <div class="modal" id="img11">
+     <h3><marquee bgcolor="black" behavior="alternate">SUETER AZUL OBSCURO 25$</marquee></h3>
+       <h3><marquee bgcolor="black" behavior="alternate">PANTALON DE TELA AZUL CLARO 45$</marquee></h3>
+       <h3><marquee bgcolor="black" behavior="alternate">ZAPATOS ACTUR AZUL 35$</marquee></h3>
+
+         <div class="imagen">
+            <a href="#img10">&#60;</a>
+            <a href="#img12"><img src="img/29.png" ></a>
+            <a href="#img12">></a>
+         </div>
+          <a class="cerrar" href="">X</a>
+
+         <div class="modal" id="img12">
+     <h3><marquee bgcolor="black" behavior="alternate">SUETER AZUL OBSCURO 25$</marquee></h3>
+       <h3><marquee bgcolor="black" behavior="alternate">PANTALON DE TELA AZUL CLARO 45$</marquee></h3>
+       <h3><marquee bgcolor="black" behavior="alternate">ZAPATOS ACTUR AZUL 35$</marquee></h3>
+
+         <div class="imagen">
+            <a href="#img11">&#60;</a>
+            <a href="#img13"><img src="img/28.png" ></a>
+            <a href="#img13">></a>
+         </div>
+          <a class="cerrar" href="">X</a>
+
+         <div class="modal" id="img13">
+     <h3><marquee bgcolor="black" behavior="alternate">SUETER AZUL OBSCURO 25$</marquee></h3>
+       <h3><marquee bgcolor="black" behavior="alternate">PANTALON DE TELA AZUL CLARO 45$</marquee></h3>
+       <h3><marquee bgcolor="black" behavior="alternate">ZAPATOS ACTUR AZUL 35$</marquee></h3>
+
+         <div class="imagen">
+            <a href="#img12">&#60;</a>
+            <a href="#img14"><img src="img/27.png" ></a>
+            <a href="#img14">></a>
+         </div>
+          <a class="cerrar" href="">X</a>
+         <div class="modal" id="img14">
+     <h3><marquee bgcolor="black" behavior="alternate">SUETER AZUL OBSCURO 25$</marquee></h3>
+       <h3><marquee bgcolor="black" behavior="alternate">PANTALON DE TELA AZUL CLARO 45$</marquee></h3>
+       <h3><marquee bgcolor="black" behavior="alternate">ZAPATOS ACTUR AZUL 35$</marquee></h3>
+
+         <div class="imagen">
+            <a href="#img13">&#60;</a>
+            <a href="#img15"><img src="img/26.png" ></a>
+            <a href="#img15">></a>
+         </div>
+          <a class="cerrar" href="">X</a>
+         <div class="modal" id="img15">
+     <h3><marquee bgcolor="black" behavior="alternate">SUETER AZUL OBSCURO 25$</marquee></h3>
+       <h3><marquee bgcolor="black" behavior="alternate">PANTALON DE TELA AZUL CLARO 45$</marquee></h3>
+       <h3><marquee bgcolor="black" behavior="alternate">ZAPATOS ACTUR AZUL 35$</marquee></h3>
+
+         <div class="imagen">
+            <a href="#img14">&#60;</a>
+            <a href="#img16"><img src="img/25.png" ></a>
+            <a href="#img16">></a>
+         </div>
+          <a class="cerrar" href="">X</a>
+         <div class="modal" id="img16">
+     <h3><marquee bgcolor="black" behavior="alternate">SUETER AZUL OBSCURO 25$</marquee></h3>
+       <h3><marquee bgcolor="black" behavior="alternate">PANTALON DE TELA AZUL CLARO 45$</marquee></h3>
+       <h3><marquee bgcolor="black" behavior="alternate">ZAPATOS ACTUR AZUL 35$</marquee></h3>
+
+         <div class="imagen">
+            <a href="#img15">&#60;</a>
+            <a href="#img17"><img src="img/24.png" ></a>
+            <a href="#img17">></a>
+         </div>
+          <a class="cerrar" href="">X</a>
+         <div class="modal" id="img17">
+     <h3><marquee bgcolor="black" behavior="alternate">SUETER AZUL OBSCURO 25$</marquee></h3>
+       <h3><marquee bgcolor="black" behavior="alternate">PANTALON DE TELA AZUL CLARO 45$</marquee></h3>
+       <h3><marquee bgcolor="black" behavior="alternate">ZAPATOS ACTUR AZUL 35$</marquee></h3>
+
+         <div class="imagen">
+            <a href="#img16">&#60;</a>
+            <a href="#img18"><img src="img/23.png" ></a>
+            <a href="#img18">></a>
+         </div>
+          <a class="cerrar" href="">X</a>
+         <div class="modal" id="img18">
+     <h3><marquee bgcolor="black" behavior="alternate">SUETER AZUL OBSCURO 25$</marquee></h3>
+       <h3><marquee bgcolor="black" behavior="alternate">PANTALON DE TELA AZUL CLARO 45$</marquee></h3>
+       <h3><marquee bgcolor="black" behavior="alternate">ZAPATOS ACTUR AZUL 35$</marquee></h3>
+
+         <div class="imagen">
+            <a href="#img17">&#60;</a>
+            <a href="#img19"><img src="img/22.png" ></a>
+            <a href="#img19">></a>
+         </div>
+          <a class="cerrar" href="">X</a>
+         <div class="modal" id="img19">
+     <h3><marquee bgcolor="black" behavior="alternate">SUETER AZUL OBSCURO 25$</marquee></h3>
+       <h3><marquee bgcolor="black" behavior="alternate">PANTALON DE TELA AZUL CLARO 45$</marquee></h3>
+       <h3><marquee bgcolor="black" behavior="alternate">ZAPATOS ACTUR AZUL 35$</marquee></h3>
+
+         <div class="imagen">
+            <a href="#img18">&#60;</a>
+            <a href="#img20"><img src="img/21.png" ></a>
+            <a href="#img20">></a>
+         </div>
+          <a class="cerrar" href="">X</a>
+         <div class="modal" id="img20">
+     <h3><marquee bgcolor="black" behavior="alternate">SUETER AZUL OBSCURO 25$</marquee></h3>
+       <h3><marquee bgcolor="black" behavior="alternate">PANTALON DE TELA AZUL CLARO 45$</marquee></h3>
+       <h3><marquee bgcolor="black" behavior="alternate">ZAPATOS ACTUR AZUL 35$</marquee></h3>
+
+         <div class="imagen">
+            <a href="#img19">&#60;</a>
+            <a href="#img1"><img src="img/30.png" ></a>
+            <a href="#img1">></a>
+         </div>
+          <a class="cerrar" href="">X</a>
+
+</body>
+</html>
