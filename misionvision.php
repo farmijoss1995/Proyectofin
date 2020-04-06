fb7901dea596b2c55f35b4330389bfe66bdeedb4
farmijoss1995 <marceloas@hotmail.es>
2020-04-06 13:04:29 -0500

    misionvision.php

---
+++ misionvision.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<title>MANGO BOUTIQUE</title>
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<link href="styles.css" rel="stylesheet" type="text/css" media="screen" />
+</head>
+<body>
+
+<div id="fb-root"></div>
+<script async defer crossorigin="anonymous" src="https://connect.facebook.net/es_ES/sdk.js#xfbml=1&version=v6.0"></script>
+
+<div id="bg">
+
+<!-- header begins -->
+<div id="header">
+	<div id="logo">
+       <img src="images/2.png">
+
+        <h1><a href="#">MANGO BOUTIQUE</a>
+        </h1></div>
+	<div id="buttons">
+		<ul>
+			<li class="first"><a href="index.php"  title="">Inicio</a></li>
+			<li><a href="nosotros.php" title="">Nosotros</a></li>
+			<li><a href="telefono.php" title="">Telefonos</a></li>
+			<li><a href="locales.php" title="">Locales</a></li>
+            <li><a href="iniciarprincipal.php"
+            title="">Vision y Mision</a></li>
+            <li><a href="misionvision.php"
+            title="">Iniciar Sesión</a></li>
+		</ul>
+	</div>
+</div>
+<!-- header ends -->
+
+<div id="main">
+<div id="maintop">
+<!--img src="images/1.png" alt="" /></div>
+<!-- content begins -->
+<div id="content">
+	<div id="right">
+    	<div id="archives">
+
+
+        </div>
+     </div>
+        <center>
+         <div id="left">
+        <div class="text">
+
+                <div style="background: white">Mango es una cadena de ropa que comunica sobre sus objetivos. Aquí presentamos sus misión, visión y valores.
+                    <br>
+                    <br>
+Misión
+<br>
+                    <br>
+Vestir a una mujer moderna y urbana / Ser una cadena de moda con espíritu de boutique / Estar presente en todas la ciudades del mundo
+
+Distribución minorista textil y complementos a nivel mundial
+
+                    <br>
+                    <br>
+ Visión
+<br>
+                    <br>
+El Informe de Sostenibilidad establece públicamente los compromisos de Responsabilidad Social Corporativa y las actividades de MANGO en los tres aspectos: económico-financiero, social y ambiental
+
+Con el fin de satisfacer los grupos de interés, el departamento de la responsabilidad social corporativa ha puesto en marcha muchos proyectos y políticas. La buena gestión de nuestros proveedores, la acción social y proyectos ambientales son las acciones que la empresa se preocupa más específicamente.
+
+
+
+
+
+
+
+<div class="fb-comments" data-href="https://developers.facebook.com/docs/plugins/comments#configurator" data-width="100%" data-numposts="10"></div>
+</div>
+
+
+
+
+     </div>
+	 </div>
+            </center>
+
+
+
+
+	<div style="clear: both"></div>
+
+</div>
+
+<!-- content ends -->
+<!--<div id="mainbot"><img src="images/spaser.gif" alt="" /></div>-->
+</div>
+
+</div>
+</body>
+</html>
+
